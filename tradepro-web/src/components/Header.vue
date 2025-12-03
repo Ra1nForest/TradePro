3dcdd209ea52f11c055d006784fb509a441ad523
Ra1nForest <1226441618@qq.com>
2025-12-03 14:45:27 +1100

    Add files via upload

---
+++ tradepro-web/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>王小虎</span>
+    <el-dropdown>
+      <i class="el-icon-arrow-down" style="margin: 15px"></i>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
+        <el-dropdown-item @click.native="logout">安全退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name : "Header",
+  methods:{
+    toUser() {
+      console.log("toUser");
+    },
+    logout() {
+      console.log("logout");
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ tradepro-web/src/components/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="overflow:auto;height: 100vh">
+    <el-container style="height: 100vh; border: 1px solid #eee">
+      <el-aside style="width:15%;min-width: 20vh; background-color: rgb(238, 241, 246)">
+        <Aside></Aside>
+      </el-aside>
+      <div style="height: 100%;width: 100%">
+        <el-container>
+          <el-header style="text-align: right; font-size: 12px">
+            <Header></Header>
+          </el-header>
+          <el-main>
+            <Main></Main>
+          </el-main>
+        </el-container>
+      </div>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Aside from "./Aside.vue";
+import Header from "@/components/Header.vue";
+import Main from "@/components/Main.vue";
+
+export default {
+  name: "Index",
+  components:{
+    Aside,
+    Header,
+    Main
+  },
+
+};
+</script>
+
+<style>
+
+.el-aside {
+  color: #333;
+}
+
+
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+</style>
